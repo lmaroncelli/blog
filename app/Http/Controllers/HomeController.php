75b2f347b7293ff9cd1785e19b560af723ed7e43
luigi <lmaroncelli@gmail.com>
2019-10-02 10:50:12 +0200

    home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $articoli = Article::orderBy('updated_at', 'desc')->get();
+        return view('home', compact('articoli'));
     }
 }
---
+++ resources/views/show_articolo.blade.php
@@ -0,0 +1,15 @@
+<div class="post-details">
+  <div class="post-meta d-flex justify-content-between">
+    <div class="date meta-last">{{$articolo->created_at->locale('it_IT')->format('d F')}} | {{$articolo->created_at->format('Y')}}</div>
+    <div class="category"><a href="#">{{$articolo->getCategorie()}}</a></div>
+  </div>
+  <a href="post.html">
+    <h3 class="h4">{{$articolo->titolo}}</h3></a>
+  <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+  <footer class="post-footer d-flex align-items-center">
+    <div class="date">
+      <i class="icon-clock"></i> {{$articolo->updated_at->locale('it_IT')->diffForHumans()}}
+    </div>
+    <span class="comments">ultimo aggiornamento</span>
+  </footer>
+</div>
